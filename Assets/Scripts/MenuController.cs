b10d9feb42310ae0feb997dd93cafff4ac7c77f1
Lucas_Serodio <53245868+L-Serodio@users.noreply.github.com>
2021-03-03 17:58:09 -0300

    Note class created

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 public class MenuController : MonoBehaviour
 {
     
    [SerializeField] private GameObject btnWrite;
+   [SerializeField] private InputField noteText;
+   [SerializeField] private GameObject panelForWrite;
+    private String textN = "";
+    
+    public void createNote() {
+        textN = noteText.text;
+        Note note = new Note(textN);
+        saveNote(note);
+    }
 
+    private void saveNoteAsJSON(Note note) {
+        
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Globalization;
+
+public class Note {
+    
+    private String text;
+    private String date;
+    public Note(String text) { 
+        this.text = text;
+        this.date = dateNow();
+    }
+
+    private String dateNow() {
+        var culture = new CultureInfo("pt-BR");
+        DateTime dateTime = DateTime.Now;
+        return dateTime.ToString(culture);
+    }
+
+
+}
